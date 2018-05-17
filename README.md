73383b9a60259637662f40460d6f41c5c359645b
Grzegorz Sarzynski <greg.syz@gmail.com>
2018-05-17 14:55:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Webpack 4 SCSS-ES8-jQuery-Bootstrap starter
+- Frontend starter: HTML + SCSS + ES8 + jQyery + Bootstrap 4
+- Image optimisation
+- Quality tools: ESLing + stylelint config files
+
+## Installation
+```
+npm install
+```
+## Usage
+`npm run dev` - development build<br>
+`npm run build` - production build
---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = (env, options) => {
+  const devMode = options.mode === 'development';
+  return {
+    devtool: devMode ? 'inline-source-map' : 'none',
+    entry: {
+      vendor: [
+        require.resolve('jquery'),
+        require.resolve('popper.js'),
+        require.resolve('bootstrap'),
+        'bootstrap/dist/css/bootstrap.min.css',
+      ],
+      app: [
+        './src/js/app.js',
+        './src/scss/main.scss',
+      ],
+    },
+    output: {
+      filename: './js/[name].js',
+    },
+    externals: {
+      jquery: 'jQuery',
+    },
+    module: {
+      rules: [
+        {
+          test: require.resolve('jquery'),
+          use: [{
+            loader: 'expose-loader',
+            options: 'jQuery',
+          }, {
+            loader: 'expose-loader',
+            options: '$',
+          }],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            devMode ? 'css-loader' : { loader: 'css-loader', options: { minimize: true } },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: devMode ? ['html-loader'] : [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+                name: './css/[name].[hash].[ext]',
+                context: '',
+              },
+            }],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: './img/[name].[hash].[ext]',
+                context: '',
+              },
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+                mozjpeg: {
+                  progressive: false,
+                  quality: 65,
+                },
+                optipng: {
+                  enabled: true,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: './css/[name].[hash].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
+  };
+};
+
